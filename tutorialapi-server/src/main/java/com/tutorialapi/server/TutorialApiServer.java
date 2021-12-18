6ebdcf4bb2a97671a8e0e833386a06b235442a0b
Phil Eagleson <phileagleson@icloud.com>
2021-12-18 15:00:35 -0500

    add TutorialApiServer main class

---
+++ tutorialapi-server/src/main/java/com/tutorialapi/server/TutorialApiServer.java
@@ -0,0 +1,12 @@
+package com.tutorialapi.server;
+import org.eclipse.jetty.http.HttpScheme;
+import org.eclipse.jetty.server.HttpConfiguration;
+
+public class TutorialApiServer {
+    public static void main(String... args) {
+        System.out.println("Hello World");
+
+        HttpConfiguration httpsConfiguration = new HttpConfiguration();
+        httpsConfiguration.setSecureScheme(HttpScheme.HTTPS.asString());
+    }
+}
