bbd13ff9a7629913541db943b617520d351d81e1
Phil Eagleson <phileagleson@icloud.com>
2022-01-08 13:37:33 -0500

    updated resource test, added json support

--- tutorialapi-rest/src/test/java/com/tutorialapi/rest/resource/HelloResourceIT.java
+++ tutorialapi-rest/src/test/java/com/tutorialapi/rest/resource/HelloResourceIT.java
@@ -2,8 +2,10 @@ package com.tutorialapi.rest.resource;
 
 import com.tutorialapi.model.Subscription;
 import com.tutorialapi.rest.ApiApplication;
+import com.tutorialapi.rest.exception.ErrorResponse;
 import com.tutorialapi.rest.security.SecurityHeader;
 import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.util.logging.LogManager;
 import org.glassfish.jersey.test.JerseyTest;
@@ -11,64 +13,81 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HelloResourceIT extends JerseyTest {
-    static {
-        LogManager.getLogManager().reset();
-    }
+	static {
+		LogManager.getLogManager().reset();
+	}
 
-    @Override
-    protected Application configure() {
-        return new ApiApplication();
-    }
+	@Override
+	protected Application configure() {
+		return new ApiApplication();
+	}
 
-    @Test
-    public void testNoSecurityHeaders() {
-        Response response = target("/test").request().get();
-        Assertions.assertEquals(401, response.getStatus());
-        Assertions.assertEquals("", response.readEntity(String.class));
-    }
+	@Test
+	public void testNoSecurityHeaders() {
+		Response response = target("/test").request().get();
 
-    @Test
-    public void testOnlyProxySecretHeader() {
-        Response response = target("/test")
-            .request()
-            .header(SecurityHeader.RAPID_API_PROXY_SECRET.getHeader(), "proxy-secret")
-            .get();
-        Assertions.assertEquals(401, response.getStatus());
-        Assertions.assertEquals("", response.readEntity(String.class));
-    }
+		Assertions.assertEquals(401, response.getStatus());
+		Assertions.assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
 
-    @Test
-    public void testOnlyProxySecretAndUserHeader() {
-        Response response = target("/test")
-            .request()
-            .header(SecurityHeader.RAPID_API_PROXY_SECRET.getHeader(), "proxy-secret")
-            .header(SecurityHeader.RAPID_API_USER.getHeader(), "user")
-            .get();
-        Assertions.assertEquals(401, response.getStatus());
-        Assertions.assertEquals("", response.readEntity(String.class));
-    }
+		ErrorResponse errorResponse = response.readEntity(ErrorResponse.class);
+		Assertions.assertEquals(401, errorResponse.getStatus());
+		Assertions.assertEquals("Missing security header: X-RapidAPI-Proxy-Secret", errorResponse.getMessage());
+	}
 
-    @Test
-    public void testOnlyInvalidSubscription() {
-        Response response = target("/test")
-            .request()
-            .header(SecurityHeader.RAPID_API_PROXY_SECRET.getHeader(), "proxy-secret")
-            .header(SecurityHeader.RAPID_API_USER.getHeader(), "user")
-            .header(SecurityHeader.RAPID_API_SUBSCRIPTION.getHeader(), "invalid")
-            .get();
-        Assertions.assertEquals(401, response.getStatus());
-        Assertions.assertEquals("", response.readEntity(String.class));
-    }
+	@Test
+	public void testOnlyProxySecretHeader() {
+		Response response = target("/test")
+		                    .request()
+		                    .header(SecurityHeader.RAPID_API_PROXY_SECRET.getHeader(), "proxy-secret")
+		                    .get();
+		Assertions.assertEquals(401, response.getStatus());
+		Assertions.assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
 
-    @Test
-    public void testValidHeaders() {
-        Response response = target("/test")
-            .request()
-            .header(SecurityHeader.RAPID_API_PROXY_SECRET.getHeader(), "proxy-secret")
-            .header(SecurityHeader.RAPID_API_USER.getHeader(), "user")
-            .header(SecurityHeader.RAPID_API_SUBSCRIPTION.getHeader(), Subscription.BASIC.name())
-            .get();
-        Assertions.assertEquals(200, response.getStatus());
-        Assertions.assertEquals("Hello from test", response.readEntity(String.class));
-    }
+		ErrorResponse errorResponse = response.readEntity(ErrorResponse.class);
+		Assertions.assertEquals(401, errorResponse.getStatus());
+		Assertions.assertEquals("Missing security header: X-RapidAPI-User", errorResponse.getMessage());
+	}
+
+	@Test
+	public void testProxySecretAndUserHeader() {
+		Response response = target("/test")
+		                    .request()
+		                    .header(SecurityHeader.RAPID_API_PROXY_SECRET.getHeader(), "proxy-secret")
+		                    .header(SecurityHeader.RAPID_API_USER.getHeader(), "user")
+		                    .get();
+		Assertions.assertEquals(401, response.getStatus());
+		Assertions.assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+
+		ErrorResponse errorResponse = response.readEntity(ErrorResponse.class);
+		Assertions.assertEquals(401, errorResponse.getStatus());
+		Assertions.assertEquals("Missing or invalid security header: X-RapidAPI-Subscription", errorResponse.getMessage());
+	}
+
+	@Test
+	public void testInvalidSubscription() {
+		Response response = target("/test")
+		                    .request()
+		                    .header(SecurityHeader.RAPID_API_PROXY_SECRET.getHeader(), "proxy-secret")
+		                    .header(SecurityHeader.RAPID_API_USER.getHeader(), "user")
+		                    .header(SecurityHeader.RAPID_API_SUBSCRIPTION.getHeader(), "invalid")
+		                    .get();
+		Assertions.assertEquals(401, response.getStatus());
+		Assertions.assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+
+		ErrorResponse errorResponse = response.readEntity(ErrorResponse.class);
+		Assertions.assertEquals(401, errorResponse.getStatus());
+		Assertions.assertEquals("Missing or invalid security header: X-RapidAPI-Subscription", errorResponse.getMessage());
+	}
+
+	@Test
+	public void testValidHeaders() {
+		Response response = target("/test")
+		                    .request()
+		                    .header(SecurityHeader.RAPID_API_PROXY_SECRET.getHeader(), "proxy-secret")
+		                    .header(SecurityHeader.RAPID_API_USER.getHeader(), "user")
+		                    .header(SecurityHeader.RAPID_API_SUBSCRIPTION.getHeader(), Subscription.BASIC.name())
+		                    .get();
+		Assertions.assertEquals(200, response.getStatus());
+		Assertions.assertEquals("Hello from test", response.readEntity(String.class));
+	}
 }
