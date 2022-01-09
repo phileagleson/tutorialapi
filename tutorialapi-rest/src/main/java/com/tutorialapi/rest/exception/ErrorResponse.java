3caadff0b1c16fd41f834494036341c8c41d955b
Phil Eagleson <phileagleson@icloud.com>
2022-01-08 22:12:52 -0500

    added tests for error response and exception mapper

--- tutorialapi-rest/src/main/java/com/tutorialapi/rest/exception/ErrorResponse.java
+++ tutorialapi-rest/src/main/java/com/tutorialapi/rest/exception/ErrorResponse.java
@@ -1,5 +1,7 @@
 package com.tutorialapi.rest.exception;
 
+import java.util.Objects;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -20,5 +22,26 @@ public class ErrorResponse {
 
 	public String getMessage() {
 		return message;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ErrorResponse that = (ErrorResponse) o;
+		return status == that.status && Objects.equals(message, that.message);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(status, message);
+	}
+
+	@Override
+	public String toString() {
+		return "ErrorResponse{" +
+		       "status=" + status +
+		       ", message='" + message + '\'' +
+		       '}';
 	}
 }
---
+++ tutorialapi-rest/src/test/java/com/tutorialapi/rest/exception/WebApplicationExceptionMapperTest.java
@@ -0,0 +1,21 @@
+package com.tutorialapi.rest.exception;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+public class WebApplicationExceptionMapperTest {
+	@Test
+	public void test() {
+		WebApplicationException exception = new NotFoundException("not found");
+		Response response = new WebApplicationExceptionMapper().toResponse(exception);
+
+		Assertions.assertEquals(404, response.getStatus());
+		Assertions.assertEquals(MediaType.APPLICATION_JSON_TYPE, response.getMediaType());
+		Assertions.assertEquals(new ErrorResponse(404, "not found"), response.getEntity());
+	}
+}
